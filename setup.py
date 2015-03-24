15275312cb265f7eb86d79b338802bafcb30508b
Alasdair Nicol <alasdair@thenicols.net>
2015-03-24 13:11:40 +0000

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='django_genshi_template',
+      version='0.1.0',
+      packages=['django_genshi_template', 'django_genshi_template.backends'],
+      )
